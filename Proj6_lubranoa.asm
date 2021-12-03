9fe39bf0292e8f574d40451e108d639da219ee29
lubranoa <lubranoa@oregonstate.edu>
2021-12-03 09:35:59 -0600

    Add display strings to .data section

--- Proj6_lubranoa.asm
+++ Proj6_lubranoa.asm
@@ -48,8 +48,20 @@ ENDM
 ; (insert constant definitions here)
 
 .data
+; Printing strings
+titleStr	BYTE	"CS 271 ASSIGNMENT 6: Designing low-level I/O Procedures",13,10,"Written by: Alexander Lubrano",0
+introStr	BYTE	"Please provide 10 signed decimal integers",13,10,
+					"Each number needs to be small enough to fit inside aaa 32-bit register. After you have",13,10,
+					"finished entering the raw numbers, this program will display a list of the integers, their",13,10,
+					"sum, and their average value.",0
+promptStr	BYTE	"Please enter a signed integer: ",0
+errorStr	BYTE	"ERROR: You did not enter a signed integer or your integer was too big.",13,10,"Please try again: ",0
+listStr		BYTE	"You entered the following numbers: ",13,10,0
+sumStr		BYTE	"The sum of these numbers is: ",0
+avgStr		BYTE	"The truncated average is: ",0
+byeStr		BYTE	"Hope this was fun!",13,10,"- Alex",0
+; Data Storage
 
-; (insert variable definitions here)
 
 .code
 main PROC
